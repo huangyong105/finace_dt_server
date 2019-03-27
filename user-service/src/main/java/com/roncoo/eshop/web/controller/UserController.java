ff9c2a4db75eb6bec8c02f5b4867a23716928da4
huangyong <huangyong@startdt.com>
2019-03-27 16:30:17 +0800

    添加配置中心

--- user-service/src/main/java/com/roncoo/eshop/web/controller/UserController.java
+++ user-service/src/main/java/com/roncoo/eshop/web/controller/UserController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,7 @@ import com.roncoo.eshop.model.User;
 import com.roncoo.eshop.service.UserService;
 
 @RestController
-@RefreshScope
+@EnableAutoConfiguration
 public class UserController {
 
 	@Value("${foo}")
@@ -35,5 +38,11 @@ public class UserController {
 		return "helloWorld"+foo;
 	}
 
+	public String getFoo() {
+		return foo;
+	}
 
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
 }
