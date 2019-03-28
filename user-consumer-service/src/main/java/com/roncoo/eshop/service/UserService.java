52980ca7f911fde066d7d9b0885ff6e8b72cf602
huangyong <huangyong@startdt.com>
2019-03-28 16:33:27 +0800

    添加用户信息

--- user-consumer-service/src/main/java/com/roncoo/eshop/service/UserService.java
+++ user-consumer-service/src/main/java/com/roncoo/eshop/service/UserService.java
@@ -3,6 +3,8 @@ package com.roncoo.eshop.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 
 import com.roncoo.eshop.mapper.UserMapper;
@@ -16,14 +18,31 @@ public class UserService {
 	private UserMapper userMapper;
 
 	@Autowired
+	LoadBalancerClient loadBalancerClient;
+
+	@Autowired
 	RestTemplate restTemplate;
 
 	public String hiService() {
-		return restTemplate.getForObject("http://USER-SERVICE/helloWorld",String.class);
+		return restTemplate.getForObject(getUrl("user-service", "/helloWorld"),String.class);
 	}
 	
 	public List<User> findAllUsers() {
 		return userMapper.findAllUsers();
 	}
-	
+
+	/**
+	 * 获取指定url
+	 * @param clientApplicationName 指定的服务提供名
+	 * @param interfaceName 需要消费的接口名
+	 * @return
+	 */
+	private String getUrl(String clientApplicationName, String interfaceName) {
+		// 使用loadBalancerClient的choose函数来负载均衡的选出一个eurekaClient的服务实例
+		ServiceInstance serviceInstance = loadBalancerClient.choose(clientApplicationName);
+		// 获取之前eurekaClient /all接口地址
+		String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + interfaceName;
+		System.out.println(url);
+		return url;
+	}
 }
