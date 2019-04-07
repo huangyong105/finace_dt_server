be51dd5b876ad747663141856d22640919919037
huangyong <huangyong@startdt.com>
2019-04-07 13:30:08 +0800

    添加登录授权

--- edge-service/src/main/java/cn/com/taiji/EdgeServiceApplication.java
+++ edge-service/src/main/java/cn/com/taiji/EdgeServiceApplication.java
@@ -3,10 +3,9 @@ package cn.com.taiji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
 /**
  * 网关服务<br/>
  * OAuth2 Resource Servers, use a Spring Security filter that authenticates requests <strong>via an incoming OAuth2 token.</strong>
@@ -15,8 +14,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableResourceServer
 @EnableHystrix
+@EnableFeignClients
 public class EdgeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
