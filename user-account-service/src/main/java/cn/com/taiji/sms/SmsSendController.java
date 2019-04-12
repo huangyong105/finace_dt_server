c10b2126665ac54dacd4407ea56c54f78e392224
huangyong <huangyong@startdt.com>
2019-04-13 01:42:19 +0800

    解决bug

--- user-account-service/src/main/java/cn/com/taiji/sms/SmsSendController.java
+++ user-account-service/src/main/java/cn/com/taiji/sms/SmsSendController.java
@@ -1,11 +1,15 @@
 package cn.com.taiji.sms;
 
 import cn.com.taiji.code.CaptchaRender;
+import cn.com.taiji.data.Result;
 import com.aliyuncs.exceptions.ClientException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * 功能描述: <br>
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SmsSendController {
 
+    Logger log = LoggerFactory.getLogger(SmsSendController.class);
     @Autowired
     private CaptchaRender render;
 
@@ -25,21 +30,39 @@ public class SmsSendController {
      * @throws ClientException
      */
     @PostMapping("/sendRegisterSmsCode")
-    public void sendRegisterSmsCode (@RequestParam(value ="mobile") String mobile) throws ClientException {
+    public Result sendRegisterSmsCode (@RequestParam(value ="mobile") String mobile)  {
         //注册账户
-        render.render(mobile,1);
+        try {
+            render.render(mobile,1);
+        } catch (ClientException e) {
+            log.info("找回密码信息异常:{}",e);
+            return Result.failure("-1","验证码发送失败");
+        }
+        return Result.success(null);
     }
 
     @PostMapping("/sendChangePasswordSmsCode")
-    public void sendChangePasswordSmsCode (@RequestParam(value ="mobile") String mobile) throws ClientException {
+    public Result sendChangePasswordSmsCode (@RequestParam(value ="mobile") String mobile) {
         //改变密码
-        render.render(mobile,2);
+        try {
+            render.render(mobile,2);
+        } catch (ClientException e) {
+            log.info("找回密码信息异常:{}",e);
+            return Result.failure("-1","验证码发送失败");
+        }
+        return Result.success(null);
     }
 
     @PostMapping("/sendFindPasswordSmsCode")
-    public void sendFindPasswordSmsCode (@RequestParam(value ="mobile") String mobile) throws ClientException {
+    public Result sendFindPasswordSmsCode (@RequestParam(value ="mobile") String mobile)  {
         //找回密码
-        render.render(mobile,3);
+        try {
+            render.render(mobile,3);
+        } catch (ClientException e) {
+            log.info("找回密码信息异常:{}",e);
+            return Result.failure("-1","验证码发送失败");
+        }
+        return Result.success(null);
     }
 
 }
