95065ac0a629b5783e488be6ba0badde9674866f
huangyong <huangyong@startdt.com>
2019-04-16 23:53:27 +0800

    添加

--- user-consumer-service/src/main/java/com/roncoo/eshop/client/UserClient.java
+++ user-consumer-service/src/main/java/com/roncoo/eshop/client/UserClient.java
@@ -7,9 +7,9 @@ package com.roncoo.eshop.client;
 
 
 
+
 import cn.com.taiji.data.Result;
 import cn.com.taiji.data.User;
-
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
