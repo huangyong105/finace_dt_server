dc590efd82b4ba90609ac9d8e099f51074e81efa
huangyong <huangyong@startdt.com>
2019-04-21 21:06:47 +0800

    更新邮件信息

--- user-service/src/main/java/com/jit/wxs/security/WebSecurityConfig.java
+++ user-service/src/main/java/com/jit/wxs/security/WebSecurityConfig.java
@@ -138,5 +138,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         // 设置拦截忽略文件夹，可以对静态资源放行
         web.ignoring().antMatchers("/assets/**");
+        web.ignoring().antMatchers("/pages/**");
     }
 }
