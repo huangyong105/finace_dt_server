dd0b61de61a76a767b2859fec7615547eb784d2f
chendongyuan1996 <943044485@qq.com>
2019-04-23 22:53:57 +0800

    修改图片上限

--- user-service/src/main/java/com/jit/wxs/entity/SysUserReq.java
+++ user-service/src/main/java/com/jit/wxs/entity/SysUserReq.java
@@ -30,4 +30,59 @@ public class SysUserReq implements Serializable {
 
     private String [] perms ;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String[] getPerms() {
+        return perms;
+    }
+
+    public void setPerms(String[] perms) {
+        this.perms = perms;
+    }
 }
