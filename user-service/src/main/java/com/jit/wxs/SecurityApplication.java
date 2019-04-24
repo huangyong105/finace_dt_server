73dc45732560d79a995d283cb0034b596b1b2e03
huangyong <huangyong@startdt.com>
2019-04-25 01:10:28 +0800

    us

--- user-service/src/main/java/com/jit/wxs/SecurityApplication.java
+++ user-service/src/main/java/com/jit/wxs/SecurityApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -31,6 +32,7 @@ import java.util.Set;
 @EnableScheduling
 @EnableAsync
 @EnableCaching
+@EnableFeignClients
 public class SecurityApplication {
 
     public static void main(String[] args) {
