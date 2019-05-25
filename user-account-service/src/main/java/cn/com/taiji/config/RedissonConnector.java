a63e614200822cbd80164b0d823c7680ac91ef11
huangyong <huangyong@startdt.com>
2019-05-26 01:14:06 +0800

    添加接口

--- user-account-service/src/main/java/cn/com/taiji/config/RedissonConnector.java
+++ user-account-service/src/main/java/cn/com/taiji/config/RedissonConnector.java
@@ -13,7 +13,7 @@ public class RedissonConnector {
     @PostConstruct
     public void init(){
         Config config = new Config();
-       config.useSingleServer().setAddress("47.101.147.30:6379");
+       config.useSingleServer().setAddress("47.112.123.113:6379");
 //        config.useMasterSlaveConnection().setMasterAddress("127.0.0.1:6379").addSlaveAddress("127.0.0.1:6389").addSlaveAddress("127.0.0.1:6399");
 //        config.useSentinelConnection().setMasterName("mymaster").addSentinelAddress("127.0.0.1:26389", "127.0.0.1:26379");
        // config.useClusterServers().addNodeAddress("39.97.173.139:7000");
